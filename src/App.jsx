376e0eabf29f0da985074102e2fa09837919a011
Collin Weaver <weaverscajunrenovations@gmail.com>
2022-12-02 10:40:37 -0600

    fixed all player API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import PlayersList from './playersList'
-// import { players } from './dummyPlayers'
-import getPlayers from './apiData'
+import fetchAllPlayers from './apiData'
 // import SinglePlayer from './singlePlayer'
 
-
-
-// const NavBar = (props) => {
-//   const players = props.players
-
-//   return (
-//     <nav>
-//       <ul>
-//         <li>Name: {players[0].name}</li>
-//         <li>About {players[0].breed}</li>
-//       </ul>
-//     </nav>
-//   )
-// }
 
 
 
 const  App = () => {
   const [players, setPlayers] = useState([]);
 
-  // const getPlayers = async () => {
-  //   try {
-  //     const response = await fetch ('https://fsa-puppy-bowl.herokuapp.com/api/2211-FTB-ET-WEB-FT/players')
-  //     const data = await response.json();
-  //     setPlayers(data);
-  //   }
-  //   catch (error){
-  //     console.error(error)
-  //   }
-  // }
+
   
   useEffect(() => {
-    getPlayers(setPlayers)
+    const getPlayers = async () =>{
+      const response = await fetchAllPlayers()
+      setPlayers(response);
+      console.log(response);
+    }
+      getPlayers();
   },[]);
 
   // console.log('this is main' , players);
 
   return (
     <>
-      {/* <div id='all-players-container' className="App"> */}
-        <PlayersList players={players}/>
-      {/* </div> */}
+        <PlayersList players={players} key={players.idx}/>
     </>
   )
 }
